063ca29f31d71b650c420fb41b83a88a4c5d44bc
Claire Davies <claire.davies@junitech.com>
2021-03-21 10:19:17 +0000

    associations corrected and category home route created

--- models/index.js
+++ models/index.js
@@ -6,11 +6,35 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+
+Product.belongsTo(Category);
+
 // Categories have many Products
+Category.hasMany(Product,);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    // Define the third table needed to store the foreign keys
+    through: {
+      model: ProductTag,
+      unique: false
+    },
+    // Define an alias for when data is retrieved
+    as: 'tagged_product'
+  });
+  
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    // Define the third table needed to store the foreign keys
+    through: {
+      model: ProductTag,
+      unique: false
+    },
+    // Define an alias for when data is retrieved
+    as: 'product_tags'
+  });
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll();
+    
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+  // be sure to include its associated Category and Tag data
+
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+     // include: [{ model: Location, through: Trip, as: 'planned_trips' }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
   // be sure to include its associated Category and Tag data
 });
 
